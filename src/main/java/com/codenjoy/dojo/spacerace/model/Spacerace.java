c094b297a2da7b4a86902c5b81ade72f14405748
OmiCrown <omicron@ukr.net>
2015-08-17 12:51:07 +0300

    minor changes in Spacerace.java

--- src/main/java/com/codenjoy/dojo/spacerace/model/Spacerace.java
+++ src/main/java/com/codenjoy/dojo/spacerace/model/Spacerace.java
@@ -24,9 +24,7 @@ public class Spacerace implements Tickable, Field {
     private List<Stone> stones;
     private List<Player> players;
     private Dice dice;
-    private boolean isNewStone = true;
     private int countStone = 0;
-    private boolean isNewBomb = true;
     private int countBomb = 0;
     private int ticksToRecharge;
     private int bulletsCount;
@@ -397,7 +395,6 @@ public class Spacerace implements Tickable, Field {
 
     public void addStone(int x) {
         stones.add(new Stone(x, size));
-        isNewStone = false;
     }
 
     public List<Stone> getStones() {
@@ -406,6 +403,5 @@ public class Spacerace implements Tickable, Field {
 
     public void addBomb(int x) {
         bombs.add(new Bomb(x, size));
-        isNewBomb = false;
     }
 }
